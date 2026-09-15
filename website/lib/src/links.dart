716df3b6d3f53fe3d6e4d26194e9310744012389
HttpAnimations <shawndouglasyt@gmail.com>
2026-09-15 09:44:04 -0700

    links 加 dist releases API 和 release 页地址

--- website/lib/src/links.dart
+++ website/lib/src/links.dart
@@ -7,6 +7,13 @@ abstract final class Links {
   static const out = 'https://gitlab.com/neosalsa/out';
   static const notes = 'https://gitlab.com/neosalsa/notes';
 
+  /// Dist project on GitLab - releases are public, assets never expire.
+  static const distProjectId = '86495557';
+  static String get releasesApi =>
+      'https://gitlab.com/api/v4/projects/$distProjectId/releases';
+  static String releasePage(String tag) =>
+      'https://gitlab.com/neosalsa/dist/-/releases/$tag';
+
   /// Canonical GitLab URL for a repo path under the group.
   static String repo(String path) => '$group/$path';
 }
